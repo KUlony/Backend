b00c3e8d0b5cfca61c9f96e22eee5da4aad602cd
tattepr <mild.4112@gmail.com>
2022-10-16 03:43:43 +0700

    finish noti

--- routers/notification.js
+++ routers/notification.js
@@ -9,7 +9,6 @@ const topicModel = require("../schemas/modeltopic");
 const followtopicModel = require("../schemas/model_following_topic");
 const likepostModel = require("../schemas/model_like_post");
 const reportpostModel = require("../schemas/model_report_post");
-const reportpostModel = require("../schemas/model_report_post");
 const noticeModel = require("../schemas/model_notification")
 
 router.get("/", async (req,res) => {
@@ -22,3 +21,23 @@ router.get("/", async (req,res) => {
       res.status(500).send({ message: e.message });
    }
 })
+
+router.get("/read/:notic_id", async (req,res) => {
+   try {
+      await noticeModel.findByIdAndUpdate(req.params.notic_id, {readed: true})
+      res.send("Readed")
+   } catch (e) {
+      res.status(500).send({ message: e.message });
+   }
+})
+
+router.get("/unread/:notic_id", async (req,res) => {
+   try {
+      await noticeModel.findByIdAndUpdate(req.params.notic_id, {readed: false})
+      res.send("Readed")
+   } catch (e) {
+      res.status(500).send({ message: e.message });
+   }
+})
+
+module.exports = router
