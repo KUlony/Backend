a3d8aa41cee6ad7374fbcd3b74d9ab9f9c569488
tattepr <mild.4112@gmail.com>
2022-11-12 03:02:47 +0700

    change res to []

--- routers/comment.js
+++ routers/comment.js
@@ -71,7 +71,7 @@ router.get("/:post_id", async (request, response) => {
   try {
     const comment = await commentModel.find({post_id : request.params.post_id,comment_status : "visible"});
     if (comment.length == 0) {
-      responese.send({message: "comment not found"})
+      responese.send([])
       return
     }
   const res = [];
