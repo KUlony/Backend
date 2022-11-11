5afaae264a6c5ff98af0a8a7f9257480ec1160fb
RainNP <parameenon@gmail.com>
2022-11-11 12:43:25 +0700

    fix get liked post

--- routers/user.js
+++ routers/user.js
@@ -113,8 +113,10 @@ router.get("/user_like_post", async (request, response) => {
   const res = []
   for(i=0 ;i<like_post.length;i++){
     const post = await postModel.find({_id : like_post[i].post_id,status : "visible"});
-    const comment = await commentModel.find({post_id : like_post[i].post_id});
-    const user = await userModel.findById(post.user_id)
+    if(post.length == 0) continue;
+    const comment = await commentModel.find({post_id : like_post[i].post_id,comment_status : "visible"});
+    const user = await userModel.findById(post[i].user_id)
+    console.log(post)
     const to_res = {
       author : {
         user_id : post.user_id,
